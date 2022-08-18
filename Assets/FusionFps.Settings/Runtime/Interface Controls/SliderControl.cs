f6dbd1cbfac9e5b49278166fb24b1663a38a8b70
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2022-08-18 20:00:58 +0100

    Fixed slider position not resetting when resetting AudioSetting to default value

--- Assets/FusionFps.Settings/Runtime/Interface Controls/SliderControl.cs
+++ Assets/FusionFps.Settings/Runtime/Interface Controls/SliderControl.cs
@@ -25,7 +25,7 @@ namespace FusionFps.Settings {
         protected override void OnSettingValueChanged(SettingBase.ValueChangeMode mode) {
             base.OnSettingValueChanged(mode);
 
-            if ( mode is SettingBase.ValueChangeMode.Deserialize or SettingBase.ValueChangeMode.Initialize ) {
+            if ( mode is not SettingBase.ValueChangeMode.Set or SettingBase.ValueChangeMode.Apply ) {
                 Debug.Log($"Setting {Setting.Name} slider to {Setting.CurrentValue}");
                 _slider.value = Setting.CurrentValue;
             }
